024ec296459f626eb809ae55cf302dbb3b332c2e
Srijay Mundlur <s2mundlu@uwaterloo.ca>
2023-05-12 21:25:10 -0400

    added images

---
+++ src/assests/index.js
@@ -0,0 +1,16 @@
+export { default as css } from "./css.png";
+export { default as cplusplus } from "./cplusplus.png";
+export { default as git } from "./git.png";
+export { default as html } from "./html.png";
+export { default as java } from "./java.png";
+export { default as javascript } from "./javascript.png";
+export { default as nodejs } from "./nodejs.png";
+export { default as react } from "./react.png";
+export { default as tailwind } from "./tailwind.png";
+export { default as python} from "./python.png";
+export { default as matlab } from "./matlab.png";
+
+
+
+
+
--- src/components/Landing/LandingSkills.js
+++ src/components/Landing/LandingSkills.js
@@ -1,29 +1,52 @@
 import React, { useState } from "react";
-
+import { css, cplusplus, git, html, java, javascript, nodejs, react, tailwind, python, matlab } from "../../assests";
 export default function LandingSkills() {
 
-  const [hover, setHover] = useState(false);
-  const onHover = () => {
-    setHover(true);
-  };
+  const proficientIn = [
+    { "image": css, name: "CSS" },
+    { "image": cplusplus, name: "C++" },
+    { "image": git, name: "Git" },
+    { "image": html, name: "HTML"},
+    { "image": java, name: "Java" },
+    { "image": javascript, name: "JavaScript"},
+    { "image": nodejs, name: "Node.js" },
+    { "image": react, name: "React" },
+    { "image": tailwind, name: "Tailwind CSS" },
+];
 
-  const onLeave = () => {
-    setHover(false);
-  };
+const semiPro = [
+  { "image": python, name: "Python" },
+  { "image": matlab, name: "MATLAB" },
+
+];
 
   return (
-    <div className='font-poppins px-72 2xl:p-96 pb-24'> 
-        <h1 className='text-2xl font-bold mb-8'>Proficient In.</h1>
-        <div className='p-6 grid-cols-5 grid gap-12'>
-            <div onMouseEnter={onHover} onMouseLeave={onLeave} className=' hover:cursor-pointer bg-gray-200 h-full w-full object-fit flex items-center justify-center relative'> 
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png" className='h-3/4 '></img>
-                {hover ? <div className="absolute bg-cyan-300 h-full w-full opacity-0 transition-opacity ease-in ease-out duration-300 hover:opacity-50 ">JavaScript</div> : <></>}
+    <div className='font-poppins px-72 2xl:px-96 pb-24'> 
+        <div>
+          <h1 className='text-2xl font-bold mb-8'>Proficient In.</h1>
+          <div className='grid-cols-5 grid gap-10'>
+          {proficientIn.map((skills) =>( 
+              <div className=' hover:cursor-pointer bg-gray-200 h-full w-full object-fit flex items-center justify-center relative'> 
+                  <img src={skills.image} className='h-4/6'></img>
+                  <div className="absolute bg-gray-200 h-full w-full opacity-0 transition-opacity ease-in ease-out duration-300 hover:opacity-90 flex justify-center items-center">
+                  <p className="text-black font-">{skills.name}</p>
+                  </div> 
+              </div>
+          ))}
+          </div>
+        </div>
+        <div>
+        <h1 className='text-2xl font-bold mb-8'>Semi Proficient In.</h1>
+        <div className='grid-cols-5 grid gap-10'>
+        {semiPro.map((skills) =>( 
+            <div className=' hover:cursor-pointer bg-gray-200 h-full w-full object-fit flex items-center justify-center relative'> 
+                <img src={skills.image} className='h-4/6'></img>
+                <div className="absolute bg-gray-200 h-full w-full opacity-0 transition-opacity ease-in ease-out duration-300 hover:opacity-90 flex justify-center items-center">
+                <p className="text-black font-">{skills.name}</p>
+                </div> 
             </div>
-            <div className='bg-black h-10 w-full'></div>
-            <div className='bg-black h-10 w-full'></div>
-            <div className='bg-black h-10 w-full'></div>
-            <div className='bg-black h-10 w-full'></div>
-            {/**ADD IMAGE THING WITH LOGOS OF SKILLS ADD FADE OUT */}
+        ))}
+        </div>
         </div>
     </div>
   )
