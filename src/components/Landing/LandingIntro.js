6c258b3967e4c99f9813412058cda9d613540724
Srijay Mundlur <s2mundlu@uwaterloo.ca>
2023-05-12 23:25:02 -0400

    updated links

--- src/components/Landing/LandingIntro.js
+++ src/components/Landing/LandingIntro.js
@@ -9,10 +9,7 @@ export default function LandingIntro() {
                 <p>I am a second year Mechatronics student at the University of Waterloo interested in a career in fullstack development.</p>
             </div>
         </div>
-        <div className='w-[400px] h-[400px] rounded-full object-cover overflow-hidden'>
-            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className=''></img>
-
-        </div>
+       
     </div>
   )
 }
--- src/components/Landing/LandingProjects.js
+++ src/components/Landing/LandingProjects.js
@@ -8,24 +8,24 @@ export default function LandingProjects() {
       <h1 className='text-2xl font-bold mb-8'>Side Projects</h1>
       <div className='grid-cols-2 grid gap-10'>
 
-        <div className="hover:cursor-pointer bg-gray-200 h-[400px] w-full object-fit flex items-center justify-center relative">
+        <a href="https://github.com/SrijayM/pintrest-sm" className="hover:cursor-pointer bg-gray-200 h-[400px] w-full object-fit flex items-center justify-center relative">
           <img src={pintrest} className='w-3/4 h-auto'/>
           <div className="absolute bg-gray-200 h-full w-full opacity-0 transition-opacity ease-in ease-out duration-300 hover:opacity-90 flex justify-center items-center">
             <p className="text-black font-bold">React Pintrest Clone</p>
           </div> 
-        </div>
-        <div className="hover:cursor-pointer bg-gray-200 h-[400px] w-full object-fit flex items-center justify-center relative">
+        </a>
+        <a href="https://github.com/SrijayM/Snake" className="hover:cursor-pointer bg-gray-200 h-[400px] w-full object-fit flex items-center justify-center relative">
           <img src={snake} className='w-3/4 h-auto'/>
           <div className="absolute bg-gray-200 h-full w-full opacity-0 transition-opacity ease-in ease-out duration-300 hover:opacity-90 flex justify-center items-center">
             <p className="text-black font-bold">Javascript Snake Game</p>
           </div> 
-        </div>
-        <div className="hover:cursor-pointer bg-gray-200 h-[400px] w-full object-fit flex items-center justify-center relative">
+        </a>
+        <a href="https://github.com/SrijayM/Run-Gunter-Run" className="hover:cursor-pointer bg-gray-200 h-[400px] w-full object-fit flex items-center justify-center relative">
           <img src={gunter} className='w-3/4 h-auto'/>
           <div className="absolute bg-gray-200 h-full w-full opacity-0 transition-opacity ease-in ease-out duration-300 hover:opacity-90 flex justify-center items-center">
             <p className="text-black font-bold">Java Obstacle Avoidance Game</p>
           </div> 
-        </div>
+        </a>
       </div>
     </div>
   )
