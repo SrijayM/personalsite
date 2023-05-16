e5f8ee2e10652fa52c9cf214ea5954d58247de34
Srijay Mundlur <@s2mundlu>
2023-05-15 22:02:28 -0400

    Updates

--- src/components/Landing/index.js
+++
@@ -1,6 +0,0 @@
-export { default as LandingHeader } from "./LandingHeader";
-export { default as LandingIntro } from "./LandingIntro";
-export { default as LandingProjects } from "./LandingProjects";
-export { default as LandingSkills } from "./LandingSkills";
-export { default as LandingWork } from "./LandingWork";
-
